006be1c289ec838e813b3c2bb516fb2e492eb43f
Michael Zinoviev <mishazinoviev91@gmail.com>
2020-11-01 12:13:36 +0200

    Cleaner code

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+// Dark or light images
+function imageMode(mode) {
+  image1.src = `img/undraw_App_installation_re_h36x_${mode}.svg`;
+  image2.src = `img/undraw_Chatting_re_j55r_${mode}.svg`;
+  image3.src = `img/undraw_Spreadsheet_re_cn18_${mode}.svg`;
+}
 // Dark mode
 function darkMode() {
   nav.style.backgroundColor = 'rgb (0 0 0 / 50% )';
   textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
   toggleIcon.children[0].textContent = 'Dark Mode';
-  toggleIcon.children[1].classList.remove('fa-sun');
-  toggleIcon.children[1].classList.add('fa-moon');
-  image1.src = 'img/undraw_App_installation_re_h36x.svg';
-  image2.src = 'img/undraw_Chatting_re_j55r.svg';
-  image3.src = 'img/undraw_Spreadsheet_re_cn18.svg';
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+  imageMode('dark');
 }
 
 // Light mode
@@ -23,11 +26,8 @@ function lightMode() {
   nav.style.backgroundColor = 'rgb (255 255 255 / 50%)';
   textBox.style.backgroundColor = 'rgb(0 0 0 / 50% )';
   toggleIcon.children[0].textContent = 'Light Mode';
-  toggleIcon.children[1].classList.remove('fa-moon');
-  toggleIcon.children[1].classList.add('fa-sun');
-  image1.src = 'img/undraw_App_installation_re_h36x_light.svg';
-  image2.src = 'img/undraw_Chatting_re_j55r_light.svg';
-  image3.src = 'img/undraw_Spreadsheet_re_cn18_light.svg';
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  imageMode('light');
 }
 
 //Switch Theme Dynamically
